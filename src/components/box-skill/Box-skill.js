bf8b8347a04a34a5821d7ea277f08b8a8534b68f
arco14 <arcodev07@gmail.com>
2025-04-11 13:36:22 -0600

    Updates

--- src/components/box-skill/Box-skill.js
+++
@@ -1,9 +0,0 @@
-const BoxSkill = ({img, text}) => {
-  return (
-    <div className="box-skill">
-      <img src={img} alt="Icon skills" />
-      <p>{text}</p>
-    </div>
-  );
-};
-export default BoxSkill;
